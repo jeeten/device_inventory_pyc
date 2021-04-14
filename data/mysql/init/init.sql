4bba01c5f809f0c004713cc4afcef0282ed8e773
jeeten <swain.jeeten@gmail.com>
2021-04-14 21:21:43 +0530

    adding mysql file

--- data/mysql/init/init.sql
+++ data/mysql/init/init.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Community v13.1.5  (64 bit)
-MySQL - 10.4.13-MariaDB-log : Database - pofgc6u59kclfyyt
+MySQL - 10.4.13-MariaDB-log : Database - device
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 10.4.13-MariaDB-log : Database - pofgc6u59kclfyyt
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`pofgc6u59kclfyyt` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`device` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 
-USE `pofgc6u59kclfyyt`;
+USE `device`;
 
 /*Table structure for table `auth_group` */
 
